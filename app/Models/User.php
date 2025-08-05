1b6d0a7a8fdf43aeaa144e799e398c6a4c6bb76a
Assiabarry <harsybarry4@gmail.com>
2025-08-05 15:37:24 +0000

    fix:user seed and product

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
+    use HasFactory;
     protected $fillable = [
         'nom', 'prenom', 'email', 'mot_de_passe',
         'telephone', 'adresse', 'ville', 'code_postal',
